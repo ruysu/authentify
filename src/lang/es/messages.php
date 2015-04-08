e6b2389b6ecc7de3297d0f424fa37c615a96b849
Luis G. Ramírez <github@gerardo.im>
2015-04-08 11:17:36 -0600

    activada on es instead of activado

--- src/lang/es/messages.php
+++ src/lang/es/messages.php
@@ -2,42 +2,42 @@
 
 return array(
 
-	'sign-in' => array(
-		'activate' => 'La cuenta no ha sido activado.',
-		'error' => 'Las credenciales no coinciden.',
-		'success' => 'Has iniciado sesión con éxito.'
-	),
+    'sign-in' => array(
+        'activate' => 'La cuenta no ha sido activada.',
+        'error' => 'Las credenciales no coinciden.',
+        'success' => 'Has iniciado sesión con éxito.',
+    ),
 
-	'activate' => array(
-		'success' => 'Tu usuario ha sido activado, ahora puedes iniciar sesión.'
-	),
+    'activate' => array(
+        'success' => 'Tu usuario ha sido activado, ahora puedes iniciar sesión.',
+    ),
 
-	'sign-up' => array(
-		'error' => 'Algo salió muy mal, por favor intenta de nuevo más tarde.',
-		'success' => 'Te has registrado con éxito, por favor sigue los pasos que enviamos a tu correo electrónico para activar tu cuenta.'
-	),
+    'sign-up' => array(
+        'error' => 'Algo salió muy mal, por favor intenta de nuevo más tarde.',
+        'success' => 'Te has registrado con éxito, por favor sigue los pasos que enviamos a tu correo electrónico para activar tu cuenta.',
+    ),
 
-	'account' => array(
-		'error' => 'Algo salió muy mal, por favor intenta de nuevo más tarde (la información de tu cuenta, no ha sido cambiada).',
-		'success' => 'Has actializado la información de tu cuenta.'
-	),
+    'account' => array(
+        'error' => 'Algo salió muy mal, por favor intenta de nuevo más tarde (la información de tu cuenta, no ha sido cambiada).',
+        'success' => 'Has actializado la información de tu cuenta.',
+    ),
 
-	'password' => array(
-		'error' => 'Algo salió muy mal, por favor intenta de nuevo más tarde, tu contraseña no se actualizó.',
-		'success' => 'Has actializado la información de tu cuenta.'
-	),
+    'password' => array(
+        'error' => 'Algo salió muy mal, por favor intenta de nuevo más tarde, tu contraseña no se actualizó.',
+        'success' => 'Has actializado la información de tu cuenta.',
+    ),
 
-	'remind' => array(
-		'error' => 'Algo salió muy mal, por favor intenta de nuevo más tarde.',
-		'success' => 'Puedes re-establecer tu contraseña siguiendo los pasos que enviamos a tuu correo electrónico.'
-	),
+    'remind' => array(
+        'error' => 'Algo salió muy mal, por favor intenta de nuevo más tarde.',
+        'success' => 'Puedes re-establecer tu contraseña siguiendo los pasos que enviamos a tuu correo electrónico.',
+    ),
 
-	'reset' => array(
-		'error' => 'Algo salió muy mal, por favor intenta de nuevo más tarde.',
-		'success' => 'Puedes utilizar tu nuevo password para iniciar sesión.'
-	),
+    'reset' => array(
+        'error' => 'Algo salió muy mal, por favor intenta de nuevo más tarde.',
+        'success' => 'Puedes utilizar tu nuevo password para iniciar sesión.',
+    ),
 
-	'change_password' => 'Cambiar contraseña',
-	'account_info' => 'Información de la cuenta',
+    'change_password' => 'Cambiar contraseña',
+    'account_info' => 'Información de la cuenta',
 
 );
